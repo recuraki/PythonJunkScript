25cf89ad331b4950efb21eac718e8d78c5accca5
Akira KANAI <kanai@wide.ad.jp>
2014-02-10 22:03:52 +0900

    sic

--- network/ntp_server_reply_spoof.py
+++ network/ntp_server_reply_spoof.py
@@ -7,39 +7,13 @@ import time
 import datetime
 import sys
 
-"""
-###[ NTP ]###
-  leap      = nowarning
-  version   = 4L
-  mode      = server
-  stratum   = 1L
-  poll      = 4L
-  precision = 235L
-  delay     = 0.0
-  dispersion= 0.0020751953125
-  id        = 1.1.1.1
-  ref       = Mon, 10 Feb 2014 07:21:50 +0000
-  orig      = Mon, 10 Feb 2014 07:22:01 +0000
-  recv      = Mon, 10 Feb 2014 07:22:00 +0000
-  sent      = Mon, 10 Feb 2014 07:22:00 +0000
-None
-"""
-
-#conf.iface = "em0"
 dstipaddr="10.1.1.1"
 
-inCurTime = int(time.time()) + 2208988800 + 1
-print "%x" % inCurTime
-inBigCurTime = int()
-inBigCurTime += (inCurTime & 0x000000ff) << 8 
-inBigCurTime += (inCurTime & 0x0000ff00) >> 8 
-inBigCurTime += (inCurTime & 0x00ff0000) << 8
-inBigCurTime += (inCurTime & 0xff000000) >> 8
-inBigCurTime = inBigCurTime << 32
-#inBigCurTime = inBigCurTime << 16
-inBigCurTime = inCurTime 
-print "%x" % inBigCurTime
-ntp = (
+
+def send_NTP():
+  inCurTime = int(time.time()) + 2208988800 + 1
+  inBigCurTime = inCurTime 
+  ntp = (
       Ether(src="11:11:11:11:11:11", dst="22:22:22:22:22:22")/
       IP(src="1.1.1.1", dst=dstipaddr)/
       UDP(sport=123,dport=123)/
@@ -58,6 +32,16 @@ ntp = (
         sent=inBigCurTime,
        )
       )
+  print(ntp["NTP"].show())
+  sendp(ntp, verbose=1, iface="em0") 
 
-print(ntp["NTP"].show())
-sendp(ntp, verbose=1, iface="em0") 
+while True:
+  time.sleep(0.1)
+  pkt = sniff(filter="port 123", count=1, timeout=1)
+  if pkt == []:
+    print("T/O")
+  else:
+    npkt = pkt[0]["NTP"]
+    print npkt.underlayer.mode
+    if npkt.mode == 3:
+      print npkt.sent
