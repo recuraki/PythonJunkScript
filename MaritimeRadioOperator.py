0bce97a27aaa7017429dd422bf375f744496ad20
kanai <kanai@wide.ad.jp>
2014-02-21 22:45:03 +0900

    通信士練習用スクリプト

---
+++ MaritimeRadioOperator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import random
+count_lines, count_per_line, count_per_sent = 5,5,5
+
+for l in range(count_lines):
+    for s in range(count_per_line):
+        st = ""
+        for c in range(count_per_sent):
+            st = st + chr(random.randint(0x41, 0x5a))
+        print st + " ",
+    print ""
+
