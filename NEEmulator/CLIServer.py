9c11d014fdb4355a2c06c8faf096628521eca942
Akira KANAI <kanai@wide.ad.jp>
2017-04-28 00:44:54 +0900

    sync

--- NEEmulator/CLIServer.py
+++ NEEmulator/CLIServer.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import socketserver
 import sys
 import yaml
+import re
 from pprint import pprint
 
 TestServerYaml="""
@@ -11,11 +12,8 @@ local:
  ipaddrlo0: "192.168.255.1"
 
 patterns:
-  - pattern: "hoge"
-    res:
-     -
-       - aaa
-       - bbb
+  - pattern: "logout"
+    action: "exit"
   - pattern: "hostname"
     res:
      -
@@ -27,9 +25,53 @@ patterns:
 """
 
 pprint(yaml.load(TestServerYaml))
+HOST, PORT = "localhost", 10000
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.curBuffer = oracle
+        while True:
+            self.data = self.request.recv(1024)
+            if self.data == b'':
+                print("EndSession")
+                return
+            print("TelnetRecv[{0}]: {1}".format( self.client_address[0], self.data))
+            self.recvCmd(self.data)
+
+    def write(self, s):
+        if isinstance(s, bytes):
+            s = s.decode(s)
+        print("Write : {0}".format(s))
+        self.request.sendall(s.encode("utf-8"))
+
+    def recvCmd(self, s):
+        s = s.decode('utf-8')
+        pprint(self.curBuffer)
+        for p in self.curBuffer:
+            pprint(p)
+            m = re.search(p["pattern"], s)
+            if m:
+                print(m)
+                return True
+        self.write("command not found: " + s)
+        return False
+    def doCmd(self, p):
+        pass
+        
+
+        
+
 
 if __name__ == "__main__":
   cfg = yaml.load(TestServerYaml)
   val = cfg.get("local", {})
+  # コマンドを待ち受ける全能のOracle
+  oracle = cfg.get("patterns", {})
   pprint(val)
+
+  # SOREUSEADDRしてLISTEN
+  socketserver.TCPServer.allow_reuse_address = True
+  server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+  # while 1
+  server.serve_forever()
 
