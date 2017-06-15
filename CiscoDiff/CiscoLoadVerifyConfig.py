10f9ef627a46d68cc5bd88cb53a39629842015b9
Akira KANAI <kanai@wide.ad.jp>
2017-06-15 16:15:15 +0900

    sync

---
+++ CiscoDiff/CiscoLoadVerifyConfig.py
@@ -0,0 +1,16 @@
+#!env python
+
+test = """
+[add]
+[delete]
+interface Loopback2
+interface Loopback2,shutdown
+interface Loopback3,shutdown
+interface Loopback4
+"""
+
+
+class
+
+if __name__ == "__main__":
+
